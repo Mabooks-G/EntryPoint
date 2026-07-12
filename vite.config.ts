cda9ca0cffd0d01e66212166c7d443d942d06299
Mabooks-G <u23708362@tuks.co.za>
2026-07-13 01:29:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://10.50.0.5:8000",   // ← CHANGE TO THIS
+        target: "http://localhost:8000",   // ← CHANGE TO THIS
         changeOrigin: true,
       },
     },
