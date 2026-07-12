c15e6a7441867cdb2e7fbc3c1f7c1e7146cc1e34
Mabooks-G <u23708362@tuks.co.za>
2026-07-13 00:49:11 +0200

    changing the target to be on AMD url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://192.168.137.1:5173",
         changeOrigin: true,
       },
     },
